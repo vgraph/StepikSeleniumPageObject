23f194d84c74aca0c62a1311cf21bfb2d47255c0
vadim at home <vadim@vgraph.ru>
2019-06-26 12:11:37 +0300

    Added test_guest_should_see_login_link. Modified BasePage class

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,15 +1,26 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage(object):
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         """Конструктор класса.
         :param WebDriver browser: The browser's driver
         :param url:
         """
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)  # Неявное ожидание со значением по умолчанию 10
 
     def open(self):
         """метод открывает нужную страницу,
         используя метод get()
         """
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
