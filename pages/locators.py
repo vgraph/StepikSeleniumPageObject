6eff05976c1cdb7e084a8158643bdcf1ff9f7dba
vadim at home <vadim@vgraph.ru>
2019-06-26 15:37:00 +0300

    Added login_page.py and form validation tests

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,13 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
+
+
+class LoginPageLocators(object):
+    LOGIN_INPUT_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_INPUT_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_BUTTON_LOGIN = (By.CSS_SELECTOR, "button[name=login_submit]")
+    REGISTER_INPUT_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_INPUT_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_INPUT_PASSWORD_CONFIRM = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON_REGISTER = (By.CSS_SELECTOR, "button[name=registration_submit]")
---
+++ pages/login_page.py
@@ -0,0 +1,34 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        url = self.browser.current_url
+        assert "login" in url
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_INPUT_EMAIL), (
+            "Login input email is not presented")
+        assert self.is_element_present(*LoginPageLocators.LOGIN_INPUT_PASSWORD), (
+            "Login input password is not presented")
+        assert self.is_element_present(*LoginPageLocators.LOGIN_BUTTON_LOGIN), (
+            "Login button is not presented")
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_EMAIL), (
+            "Register input email is not presented")
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_PASSWORD), (
+            "Register input password is not presented")
+        assert self.is_element_present(*LoginPageLocators.REGISTER_INPUT_PASSWORD_CONFIRM), (
+            "Register input password is not presented")
+        assert self.is_element_present(*LoginPageLocators.REGISTER_BUTTON_REGISTER), (
+            "Register button is not presented")
