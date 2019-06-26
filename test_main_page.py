d407617c084d0fe66e826f0e5a2b94fd020982b0
vadim at home <vadim@vgraph.ru>
2019-06-26 17:11:59 +0300

    The first implementation of the method of moving to a page

--- test_main_page.py
+++ test_main_page.py
@@ -6,7 +6,8 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+    login_page = page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
